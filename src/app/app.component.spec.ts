448881c80a7fb964370bc1e98d3c43da4db35989
Aline Dubois <juillet.aline@gmail.com>
2021-07-18 15:11:28 +0200

    création du projet films

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'movies-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('movies-angular');
+    expect(app.name).toEqual('movies-angular');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+interface User {
+  name: string;
+  lastName: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movies-angular';
+  titre = 'Mes films préférés';
 }
+
